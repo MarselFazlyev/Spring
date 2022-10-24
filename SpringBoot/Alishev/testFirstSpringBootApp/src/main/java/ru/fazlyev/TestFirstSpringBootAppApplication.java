ad28294493158ef86e165ff2aca4c9bb700c1e96
Fazlyev Marsel <fazlyev@mail.ru>
2022-10-24 12:45:31 +0300

    update

---
+++ SpringBoot/Alishev/testFirstSpringBootApp/src/main/java/ru/fazlyev/TestFirstSpringBootAppApplication.java
@@ -0,0 +1,14 @@
+package ru.fazlyev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFirstSpringBootAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TestFirstSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/Alishev/testFirstSpringBootApp/src/test/java/ru/fazlyev/testFirstSpringBootApp/TestFirstSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fazlyev.testFirstSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFirstSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
