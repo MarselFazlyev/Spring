91161330b69daaaff3be48ac6dc5830156b6c54d
Fazlyev Marsel <fazlyev@mail.ru>
2022-10-25 00:52:44 +0300

    directory SpringSecurity added

---
+++ SpringSecurity/FirstSecurityApp1/src/main/java/ru/fazlyev/FirstSecurityApp1/FirstSecurityApp1Application.java
@@ -0,0 +1,13 @@
+package ru.fazlyev.FirstSecurityApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSecurityApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSecurityApp1Application.class, args);
+	}
+
+}
---
+++ SpringSecurity/FirstSecurityApp1/src/test/java/ru/fazlyev/FirstSecurityApp1/FirstSecurityApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fazlyev.FirstSecurityApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSecurityApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
